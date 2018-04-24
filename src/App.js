a593d5b33532cc6ad44fe5fa07dd4949518c1846
pyssion <peter@wanglu.pw>
2018-04-24 13:08:16 +0800

    initial commit generated with create-react-app with some changes.

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div style={{ padding: 30 }}>
+        <div>My website.</div>
+      </div>
+    );
+  }
+}
+
+export default App;
